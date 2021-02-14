278eaa99bfc5a61f5b02e9fa03f271edf143a0a1
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-02-14 16:45:33 -0300

    feat: create validation error for yup

---
+++ src/utils/getValidationErrors.ts
@@ -0,0 +1,15 @@
+import { ValidationError } from 'yup';
+
+interface Errors {
+  [key: string]: string; // pode ser qualquer coisa desde que seja string (nome, email, password etc)
+}
+
+export default function getValidationErrors(err: ValidationError): Errors {
+  const validationErrors: Errors = {};
+
+  err.inner.forEach(error => {
+    validationErrors[error.path] = error.message;
+  });
+
+  return validationErrors;
+}
