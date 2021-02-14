0bd7a99e4d2ad327bf88e01efb30727c9cfdb0d7
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-02-14 16:52:18 -0300

    feat: update login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,95 +1,83 @@
-import styled, { keyframes } from 'styled-components';
-import { shade } from 'polished';
-
 import { useCallback, useRef } from 'react';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-const Container = styled.div`
-  height: 100vh;
-  display: flex;
-  align-items: stretch;
-`;
+import Input from '../src/components/Input/index';
 
-const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  max-width: 600px;
-`;
+import { useToast } from '../src/hooks/toast';
 
-const Background = styled.div`
-  flex: 1;
-  background: url("/assets/bg.png") no-repeat;
-  background-size: cover;
-`;
+import getValidationErrors from '../src/utils/getValidationErrors';
 
+import { Container, Background, Content, AnimationContainer } from '../styles/login';
+import { GetServerSideProps } from 'next';
+import api from '../src/services/api';
 
-const appearFromRight = keyframes`
-  from {
-    opacity: 0;
-    transform: translateX(50px);
-  }
-  to {
-    opacity: 1;
-    transform: translateX(0);
-  }
-`;
+interface User {
+  email: string;
+  password: string;
+}
+interface UserProps {
+  users: User[]
+}
 
-const AnimationContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+export default function Home({ users }: UserProps) {
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
 
-  animation: ${appearFromRight} 1s;
+  const checkLogin = useCallback((data: User) => {
+    const userExists = users.find((u) => {
+      if (u.email === data.email && u.password === data.password) {
+        return u;
+      }
+    });
 
-  form {
-    margin: 35px 0;
-    width: 340px;
-    text-align: center;
-
-    h1 {
-      margin-bottom: 15px;
+    if (userExists) {
+      addToast({
+        type: 'success',
+        title: 'Sucesso',
+        description: 'Login realizado com sucesso!',
+      });
     }
+    else {
+      addToast({
+        type: 'error',
+        title: 'Erro',
+        description: 'Ocorreu um erro ao fazer login, tente novamente.',
+      });
+    }
+  }, []);
 
-    button {
-      color: #f4ede8;
-      display: block;
-      margin-top: 24px;
-      text-decoration: none;
-      transition: color 0.2s;
+  const handleSubmit = useCallback(async (data: User) => {
+    try {
+      console.log('foi')
+      formRef.current?.setErrors({});
 
-      &:hover {
-        color: ${shade(0.2, '#f3ede8')};
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Digite um e-mail válido')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(7, 'No mínimo 7 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      checkLogin(data);
+
+    } catch (err) {
+      console.log('errou', err)
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        return;
       }
     }
-  }
-  > button {
-    color: #ff9000;
-    display: block;
-    margin-top: 24px;
-    text-decoration: none;
-    transition: color 0.2s;
 
-    display: flex;
-    align-items: center;
-
-    &:hover {
-      color: ${shade(0.2, '#ff9000')};
-    }
-  }
-`;
-
-export default function Home() {
-  const formRef = useRef<FormHandles>(null);
-
-  const handleSubmit = useCallback(() => {
-
-  }, []);
+  }, [addToast]);
 
   return (
     <Container>
@@ -98,24 +86,38 @@ export default function Home() {
         <AnimationContainer>
           <Form ref={formRef} onSubmit={handleSubmit}>
             <h1>Olá, seja bem-vindo!</h1>
-            <p>Para acessar a plataforma, faça seu login</p>
-            <input name="email" placeholder="user.name@mail.com" />
-            <input
+            <h3>Para acessar a plataforma, faça seu login.</h3>
+
+            <label htmlFor="email">E-MAIL</label>
+            <Input name="email" placeholder="user.name@mail.com" id="email" />
+
+            <label htmlFor="password">SENHA</label>
+            <Input
               name="password"
               type="password"
-              placeholder="*******"
+              placeholder="senha"
+              id="password"
             />
 
-            <button type="submit">Entrar</button>
+            <button type="submit">ENTRAR</button>
+
+            <p>Esqueceu seu login ou senha?</p>
+            <p>Clique <a href='#'>aqui</a></p>
           </Form>
 
-          <p>
-            Esqueceu seu login ou senha? Clique aqui
-            </p>
         </AnimationContainer>
 
       </Content>
     </Container>
-
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await api.get('users');
+
+  return {
+    props: {
+      users: response.data
+    }
+  }
 }
