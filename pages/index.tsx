0aa8734e916f93c2b9493d343b7921ba78750eb6
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-02-15 11:46:33 -0300

    feat: update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -88,7 +88,7 @@ export default function Home({ users }: UserProps) {
       <Background />
       <Content>
         <AnimationContainer>
-          <Form ref={formRef} onSubmit={handleSubmit}>
+          <Form ref={formRef} onSubmit={handleSubmit} translate={Form}>
             <h1>Olá, seja bem-vindo!</h1>
             <h3>Para acessar a plataforma, faça seu login.</h3>
 
